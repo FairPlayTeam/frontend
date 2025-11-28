2dc814e1e5b5e4b359f939f97adc9af3d8c281ba
spectra <nayelfourreau@gmail.com>
2025-11-28 19:08:28 +0100

    Revert "change downbar into sidebar and fix topbar"
    This reverts commit 2ca6b3949692403e76d50b861f54b60e350c8632.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,28 @@
-import { colors } from "@/components/ui/colors";
 import { Colors } from '@/constants/theme';
 import { AuthProvider, useAuth } from '@/context/auth';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { auth, profile } from '@/services/auth';
-import { FontAwesome } from "@expo/vector-icons";
+import { View, Text, Pressable, TextInput, TouchableOpacity, Image, Linking, StyleSheet } from "react-native";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import { Link, useRouter } from 'expo-router';
+import { Link,} from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { Image, Linking, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import 'react-native-reanimated';
-function Header() {
-  const { user, setUser } = useAuth();
+import { useRouter } from "expo-router";
+import { colors } from "@/components/ui/colors";
+//import {styles} from '@/components/Topbar/Topbar.module'
+function HeaderLeft() {
+    const { user, setUser } = useAuth();
   const colorScheme = useColorScheme();
   const tint = Colors[colorScheme ?? 'light'].tint;
   const isDark = (colorScheme ?? 'light') === 'dark';
-  const router = useRouter();
-
+  const router = useRouter()
   useEffect(() => {
     if (!user) {
       profile
@@ -30,95 +31,97 @@ function Header() {
         .catch(() => {});
     }
   }, [user, setUser]);
-
   async function onLogout() {
     await auth.logout();
     setUser(null);
   }
-
   return (
     <>
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <View style={[styles.header, { backgroundColor: isDark ? '#333' : colors.white}]}>
-          <View style={styles.container}>
+<View style={styles.header}>
+            <View style={styles.container}>
+                {/* Logo à gauche */}
                 <Link href={'/'} asChild>
-              <View style={{ flexDirection: "row" }}>
+                      <View style={{flexDirection: "row", }}>
                       <Image
-                  source={require("../assets/images/favicon.png")}
-                  style={{ width: 40, height: 40, borderRadius: 5 }}
+          source={require("../assets/images/favicon.png") }
+          style={{ width: 40, height: 40 ,borderRadius: 5,}}
+        /> <Text style={{
+              fontSize: 22,
+                fontWeight: "700",
+                color: "#333333",
+                paddingLeft: 12,}}>FairPlay</Text></View></Link>
+            
+                {/* Search au milieu */}
+                <View style={styles.searchBar}>
+                  <TextInput
+                    placeholder="Rechercher des vidéos"
+                    style={styles.searchInput}
                   />
-                <Text style={{
-                  fontSize: 22,
-                  fontWeight: "700",
-                  color: isDark ? "#fff" : "#333333",
-                  paddingLeft: 12,
-                }}>FairPlay</Text>
+                  <TouchableOpacity style={styles.searchButton}>
+                    <FontAwesome name="search" size={18} color="black" />
+                  </TouchableOpacity>
+                </View>
+            
+                
               </View>
-            </Link>
-            <View style={[styles.searchBar, { backgroundColor: isDark ? '#555' : colors.white }]}>
-              <TextInput
-                placeholder="Rechercher des vidéos"
-                style={[styles.searchInput, { color: isDark ? "#fff" : colors.darkGray }]}
-              />
-              <TouchableOpacity style={[styles.searchButton, { backgroundColor: isDark ? '#777' : colors.lightGray }]}>
-                <FontAwesome name="search" size={18} color={isDark ? "#fff" : "black"} />
-              </TouchableOpacity>
-            </View>
           </View>
-        
-          <View style={styles.headerActions}>
+
+    
+    <View style={{ flexDirection: 'row', gap: 12, paddingRight: 12 }}>
+      <View style={styles.headerActions}>
             {!user ? (
               <>
                 <TouchableOpacity onPress={() => router.push("/login")}>
-                  <Text style={[styles.loginButton, { color: isDark ? "#fff" : colors.darkGray }]}>Se Connecter</Text>
+                  <Text style={styles.loginButton}>Se Connecter</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => router.push("/login?register=true")}>
-                  <Text style={[styles.loginButton, { color: isDark ? "#fff" : colors.darkGray }]}>S'Inscrire</Text>
+                  <Text style={styles.loginButton}>S'Inscrire</Text>
                 </TouchableOpacity>
               </>
             ) : (
-              <View style={{ flexDirection: 'row', alignItems: 'center', gap: 12 }}>
-                <Text
-                  style={{
-                    fontWeight: '600',
-                    color: isDark ? "#fff" : Colors[colorScheme ?? 'light'].text,
-                  }}
-                >
-                  Welcome back, {user.displayName || user.username}
-                </Text>
-                <Link
-                  href="/dashboard"
-                  style={{
-                    paddingVertical: 6,
-                    paddingHorizontal: 12,
-                    borderRadius: 8,
-                    borderWidth: 1,
-                    borderColor: tint,
-                    color: tint,
-                    fontWeight: '600',
-                  }}
-                >
-                  My Account
-                </Link>
-                <Pressable
-                  onPress={onLogout}
-                  style={{
-                    backgroundColor: isDark ? '#fff' : '#000',
-                    paddingVertical: 6,
-                    paddingHorizontal: 12,
-                    borderRadius: 8,
-                    marginRight: 10,
-                  }}
-                >
-                  <Text
-                    style={{ color: isDark ? '#000' : '#fff', fontWeight: '600' }}
-                  >
-                    Logout
-                  </Text>
-                </Pressable>
-              </View>
-            )}
-
+              
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 12 }}>
+          <Text
+            style={{
+              fontWeight: '600',
+              color: Colors[colorScheme ?? 'light'].text,
+            }}
+          >
+            Welcome back, {user.displayName || user.username}
+          </Text>
+          <Link
+            href="/dashboard"
+            style={{
+              paddingVertical: 6,
+              paddingHorizontal: 12,
+              borderRadius: 8,
+              borderWidth: 1,
+              borderColor: tint,
+              color: tint,
+              fontWeight: '600',
+            }}
+          >
+            My Account
+          </Link>
+          <Pressable
+            onPress={onLogout}
+            style={{
+              backgroundColor: isDark ? '#fff' : '#000',
+              paddingVertical: 6,
+              paddingHorizontal: 12,
+              borderRadius: 8,
+              marginRight:10,
+            }}
+          >
+            <Text
+              style={{ color: isDark ? '#000' : '#fff', fontWeight: '600' }}
+            >
+              Logout
+            </Text>
+          </Pressable>
+        </View>
+      )}
+      
             <TouchableOpacity
               style={styles.donateButton}
               onPress={() => Linking.openURL("https://ko-fi.com/fairplay_")}
@@ -126,81 +129,88 @@ function Header() {
               <Text style={styles.donateText}>Donate</Text>
             </TouchableOpacity>
           </View>
-        </View>
-      </ThemeProvider>
+    </View>
     </>
-  );
+  )
 }
-
+function Sidebar(){
+  return(
+    <p>test</p>
+  )
+}
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <AuthProvider>
-          <Drawer
-            screenOptions={{
-              headerShown: true,
-              headerTitle: '',
-              headerStyle: { backgroundColor: '#fff' },
-              headerTintColor: '#fff',
-              header: () => <Header />,
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+
+      <AuthProvider>
+        
+        <Drawer
+          screenOptions={{
+            headerShown: true,
+            headerTitle: '',
+            headerStyle: { backgroundColor: '#fff' },
+            headerTintColor: '#fff',
+            Sidebar:()=><Sidebar />,
+            headerLeft: ()=><HeaderLeft />,
+          }}
+        >
+          
+          <Drawer.Screen
+            name="(tabs)"
+            options={{
+              
             }}
-          >
-            <Drawer.Screen
-              name="(tabs)"
-              options={{
-              }}
-            />
-            <Drawer.Screen name="login" />
-            <Drawer.Screen name="register" />
-            <Drawer.Screen name="dashboard" options={{ title: 'Dashboard' }} />
-            <Drawer.Screen name="video/[id]" options={{ headerShown: true }} />
-            <Drawer.Screen
-              name="people"
-              options={{ title: 'People' }}
-            />
-          </Drawer>
-        </AuthProvider>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </>
+          />
+          
+          <Drawer.Screen name="login" />
+          <Drawer.Screen name="register" />
+          <Drawer.Screen name="dashboard" options={{ title: 'Dashboard' }} />
+          <Drawer.Screen name="video/[id]" options={{ headerShown: true }} />
+          <Drawer.Screen
+            name="people"
+            options={{ presentation: 'modal', title: 'People' }}
+          />
+        </Drawer>
+        
+      </AuthProvider>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
 
 export const styles = StyleSheet.create({
   header: {
-    
+    backgroundColor: colors.white,
     borderBottomWidth: 1,
     borderColor: colors.lightGray,
     paddingVertical: 10,
-    paddingRight: 5,
-    flex: 1,
+    paddingHorizontal: 16,
+    flex:1,
     flexDirection: "row",
     alignItems: "center",
     width: "100%",
     padding: 8,
-    justifyContent: "space-between",
+ 
   },
   container: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-between", // push gauche ↔ droite
   },
   logo: {
     fontSize: 22,
     fontWeight: "700",
     color: colors.darkGray,
     paddingLeft: 20,
+    
   },
   searchBar: {
-    flex:1,
     flexDirection: "row",
-    
-    flexGrow: 1,
-    flexShrink: 1,
-    flexBasis: 520,
+    flexGrow: 1, // occupe l’espace dispo
+    flexShrink: 1, // se réduit si l’espace est petit
+    flexBasis: 520, // largeur de base (comme CSS)
     maxWidth: 720,
     marginHorizontal: 16,
     borderWidth: 1,
@@ -209,7 +219,8 @@ export const styles = StyleSheet.create({
     backgroundColor: colors.white,
     alignItems: "center",
     overflow: "hidden",
-    minWidth: 100,
+    
+    
   },
   searchInput: {
     flex: 1,
@@ -218,6 +229,7 @@ export const styles = StyleSheet.create({
     fontSize: 15,
     color: colors.darkGray,
     textAlign: "left",
+  
   },
   searchButton: {
     backgroundColor: colors.lightGray,
@@ -225,15 +237,17 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 14,
     justifyContent: "center",
     alignItems: "center",
+    
   },
   headerActions: {
     flexDirection: "row",
-    alignItems: "flex-end",
+    alignItems: "center",
+    
   },
   loginButton: {
     height: 36,
     paddingHorizontal: 14,
-    paddingVertical: 8,
+    paddingVertical:8,
     borderRadius: 8,
     borderWidth: 1,
     borderColor: colors.lightGray,
@@ -244,9 +258,10 @@ export const styles = StyleSheet.create({
     textAlign: "center",
     textAlignVertical: "center",
     marginRight: 8,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "center", // centre verticalement
+    alignItems: "center",     // centre horizontalement
     marginHorizontal: 5,
+    
   },
   donateButton: {
     backgroundColor: colors.donatePink,
@@ -262,4 +277,3 @@ export const styles = StyleSheet.create({
     fontSize: 14,
   },
 });
-
